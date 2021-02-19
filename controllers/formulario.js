59f0d251a8fd05a09d53374b2451cd937a493880
Kessyus Fófano <kessyus@gmail.com>
2021-02-18 21:29:47 -0300

    Estilizei o CSS para mobile

--- controllers/formulario.js
+++ controllers/formulario.js
@@ -5,6 +5,9 @@ const html_to_pdf = require('html-pdf-node');
 const servicesModel = require('../models/services-model');
 const bandwidthModel = require('../models/bandwidth-model');
 const states = require('../json/states.json');
+const pricesMPLS = require('../json/pricesMPLS.json');
+const pricesDIA = require('../json/pricesDIA.json');
+const pricesBBD = require('../json/pricesBBD.json')
 
 const getPage = (req, res, next) => {
 
@@ -27,8 +30,8 @@ const getPage = (req, res, next) => {
     // States
     const statesItemViewModel = states.UF.map((item) => {
         return {
-            value: item.id,
-            label: item.sigla
+            value: item.sigla,
+            label: item.nome
         }
     })
 
@@ -45,14 +48,47 @@ const getPage = (req, res, next) => {
 const postPage = (req, res, next) => {
 
     const body = req.body;
+    let priceOTCWithoutTaxes = parseFloat(body.priceOTC).toFixed(2);
+    let priceMRCWithoutTaxes = parseFloat(body.priceMRC).toFixed(2);
+
+    // Formata o preço conforme solicitação do usuário
+    // Se escolheu "1" será customizado e usará os preços digitados no form
+    // que já foram puxados nas linhas acima
+    // Se escolheu "2" será os valores da tabela do serviço
+    if (body.priceType === "2") {
+        // Verifica qual o serviço para consultar na tabela adequada
+        let newMRC = 0;
+        if (body.service === "MPLS") {
+            newMRC = pricesMPLS.find(item => (item.bandwidth === body.bandwidth)).mrc;
+        } else if (body.service === "Dedicated Internet Access") {
+            newMRC = pricesDIA.find(item => (item.bandwidth === body.bandwidth)).mrc;
+        } else if (body.service === "Broadband Internet Access") {
+            newMRC = pricesBBD.find(item => (item.bandwidth === body.bandwidth)).mrc;
+        }
+        priceMRCWithoutTaxes = parseFloat(newMRC).toFixed(2);
+        priceOTCWithoutTaxes = 1000.00;
+    }
+
+    let impostoICMS = states.UF.find(item => (item.sigla === body.state)).icms;
+    let impostoFatorDivisor = parseFloat(1 - ((impostoICMS + 3.0 + 0.65) / 100)).toFixed(4);
+    const priceOTCWithTaxes = parseFloat(priceOTCWithoutTaxes / 0.8575).toFixed(2);
+    const priceMRCWithTaxes = parseFloat(priceMRCWithoutTaxes / impostoFatorDivisor).toFixed(2);
 
     // cria a viewModel
     const postViewModel = {
-        name: body.address
-        // email: body.email,
-        // birthDate: body.birthDate,
-        // sexo: sexoModel.getSexoPorId(body.gender).descricao,
-        // estadoCivil: estadoCivilModel.getEstadoCivilPorId(body.maritalStatus).descricao
+        company: body.company,
+        name: body.name, 
+        email: body.email,
+        address: body.address,
+        city: body.city,
+        state: body.state,
+        service: body.service,
+        bandwidth: body.bandwidth,
+        priceType: body.priceType,
+        priceOTCWithoutTaxes,
+        priceMRCWithoutTaxes,
+        priceOTCWithTaxes,
+        priceMRCWithTaxes
     }
 
     // une a viewModel com o html
