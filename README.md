9a6645a14fcd87ac6f380b37790bc5a748b280be
Kessyus Fófano <kessyus@gmail.com>
2021-02-22 11:54:22 -0300

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Proposal Maker
 
-App to create a Telecommunications Business Proposal in PDF.
+Projeto da primeira etapa do Bootcamp do Infnet. O objetivo do projeto é introduzir o NodeJS e algumas bibliotecas para desenvolvimento Web.
+
+## Escopo básico do projeto:
+
+[x] Gerar um web app usando NodeJS e Express;
+[x] O aplicativo deverá permitir imprimir em PDF os campos preenchidos pelo usuário no form;
+[x] Deveremos usar CSS puro, sem nenhum tipo de SCSS;
+[x] Não haverá consulta a banco de dados;
+[x] A view engine será o EJS;
+[x] O MVC será a arquitetura do programa;
+[x] Devemos criar pelo menos dois models para gerar dinamicamente os valores de algum campo do formulário;
+[x] O desenvolvedor deverá fazer o acompanhamento das tarefas através de um quadro no Trello compartilhado com os professores, monitores e coordenador.
+
+## Projeto desenvolvido:
+
+Proposal Maker foi desenvolvido para facilitar a criação de propostas comerciais de serviços de telecomunicações em inglês. O sistema funciona tanto no browser quanto no celular. O sistema disponibiliza ainda a possibilidade de alterar os valores das tabelas de preços pré aprovados através de JSON para otimizar a geração de propostas. Ao final do cadastro, a proposta será gerada em PDF, podendo ser enviada para os clientes de forma mais rápida e acertiva.
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const formularioCtrl = require("./controllers/formulario");
-const precoCtrlMPLS = require('./controllers/precoMPLS');
-const precoCtrlDIA = require('./controllers/precoDIA');
-const precoCtrlBBD = require('./controllers/precoBBD');
+const precoCtrlMPLS = require("./controllers/precoMPLS");
+const precoCtrlDIA = require("./controllers/precoDIA");
+const precoCtrlBBD = require("./controllers/precoBBD");
 const port = process.env.PORT || 5000;
 
 // inicia o app e define alguns parâmetros
@@ -15,12 +15,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // *** ROTAS INÍCIO ***
-app.get("/", (req, res, next) => { res.render("index"); });
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
 // rotas formulario
 app.get("/formulario", formularioCtrl.getFormulario);
 app.post("/formulario", formularioCtrl.postFormulario);
 // rotas tabelas de preços
-app.get("/precos", (req, res, next) => { res.render("precos"); });
+app.get("/precos", (req, res, next) => {
+  res.render("precos");
+});
 app.get("/precosMPLS", precoCtrlMPLS.getFormulario);
 app.post("/precosMPLS", precoCtrlMPLS.postFormulario);
 app.get("/precosDIA", precoCtrlDIA.getFormulario);
@@ -28,10 +32,13 @@ app.post("/precosDIA", precoCtrlDIA.postFormulario);
 app.get("/precosBBD", precoCtrlBBD.getFormulario);
 app.post("/precosBBD", precoCtrlBBD.postFormulario);
 // rota sobre
-app.get("/sobre", (req, res, next) => { res.render("sobre"); });
+app.get("/sobre", (req, res, next) => {
+  res.render("sobre");
+});
 // *** ROTAS FIM ***
 
 // porta
 app.listen(port, () => {
   console.log("The server is running at http://localhost:" + port);
 });
+
